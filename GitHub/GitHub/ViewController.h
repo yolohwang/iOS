d8001cf4136e1f5840ea106938eae054a84d1265
yolohwang <yolo.hwang@gmail.com>
2016-06-29 17:28:30 +0800

    iOS项目
    学习

---
+++ GitHub/GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by 黄永乐 on 16/6/29.
+//  Copyright © 2016年 Yolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHub/GitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHub
+//
+//  Created by 黄永乐 on 16/6/29.
+//  Copyright © 2016年 Yolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
